ef91e4eccbe6f2bb619655581b1431b306815c43
D2R-hm <48502933+D2R-hm@users.noreply.github.com>
2021-10-17 20:20:48 +0900

    debug3

--- run_discord_bot.py
+++ run_discord_bot.py
@@ -1,19 +1,19 @@
-# # -*- coding: utf-8 -*-
-# import sys
-# import os
+# -*- coding: utf-8 -*-
+import sys
+import os
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.support.select import Select
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.alert import Alert
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
-# from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
 
-# import discord
+import discord
 
 # # WEBスクレイピング
 # def get_web_data(url, el):
